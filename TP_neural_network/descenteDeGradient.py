727d7e5e6c1b71ee105db615ab4acd2f75c028f9
SIGESI <songsixte@gmail.com>
2020-01-24 10:56:11 +0100

    input et output

---
+++ TP_neural_network/descenteDeGradient.py
@@ -0,0 +1,11 @@
+
+'''
+
+Input : Lnombre de couches cachées, Xdonnées numériques, Y Sortie du
+réseau (une matrice sparse, etc.)
+Input : Wl; 1  l  L, matrice de poids.
+Input : bl; 1  l  L, vecteur de poids.
+Output : Paramètres ajustés Wl et bl
+Initialisation ;
+
+'''
