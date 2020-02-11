a7747509a3a5d448daec5bf0827fc2124c532bfd
SIGESI <songsixte@gmail.com>
2020-02-11 10:55:48 +0100

    initiation de TP Deeplearning

---
+++ TP_deep_learning/tiny_imagenet_dataset.py
@@ -0,0 +1,50 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+import os
+import numpy as np
+import cv2
+import sys
+
+nb_classes = 20
+nb_samples_per_class = 500
+nb_samples_per_class_train = 450
+nb_samples_per_class_test = nb_samples_per_class - nb_samples_per_class_train
+
+class TinyImageNetDatasetTrain(Dataset):
+    def __init__(self, img_dir):
+        super(TinyImageNetDatasetTrain, self).__init__()
+        self.img_dir = img_dir
+
+    def __len__(self):
+        return nb_classes*nb_samples_per_class_train
+
+    def __getitem__(self, idx):
+        idx_class = idx//nb_samples_per_class_train
+        idx_sample = idx%nb_samples_per_class_train
+
+        img_path = self.img_dir + '/{:03}/{:03}.jpg'.format(idx_class, idx_sample)
+        img = cv2.imread(img_path, cv2.IMREAD_COLOR).astype(np.float32) / 255.0
+
+        img = torch.tensor(np.array(img.transpose([2, 0, 1])))
+        id_class = torch.tensor(idx_class).type(torch.int64)
+        return img, id_class
+
+class TinyImageNetDatasetTest(Dataset):
+    def __init__(self, img_dir):
+        super(TinyImageNetDatasetTest, self).__init__()
+        self.img_dir = img_dir
+
+    def __len__(self):
+        return nb_classes * nb_samples_per_class_test
+
+    def __getitem__(self, idx):
+        idx_class = idx // nb_samples_per_class_test
+        idx_sample = idx % nb_samples_per_class_test
+
+        img_path = self.img_dir + '/{:03}/{:03}.jpg'.format(idx_class, nb_samples_per_class_train + idx_sample)
+        img = cv2.imread(img_path, cv2.IMREAD_COLOR).astype(np.float32) / 255.0
+
+        img = torch.tensor(np.array(img.transpose([2, 0, 1])))
+        id_class = torch.tensor(idx_class).type(torch.int64)
+        return img, id_class
+    
